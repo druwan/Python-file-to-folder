abe292dccf67ec78be5c51756aa2e5e85ab1e1bb
Christopher <ruwanwestman@gmail.com>
2021-05-10 16:01:50 +0200

    file-to-folder

---
+++ file-to-folder.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+from pathlib import Path
+
+
+
+path = Path("D://Downloads")
+
+# Read filename and create folder
+for source_name in os.listdir("D://Downloads"):
+    if source_name.endswith(".pdf"):
+        # If pdf file - create a folder
+        source_name_no_ext = os.path.splitext(source_name)[0]
+        newFolder = os.path.join(path, source_name_no_ext)
+        # Create a path to each file to be moved
+        fileDirMove = os.path.join(path, source_name)
+        os.mkdir(newFolder)
+        shutil.move(fileDirMove, newFolder)
+
